86ffcb3d79e1ac6d7f5974ac5483dafc0bbad28b
Guus Sliepen <guus@perpendicular-dimensions.org>
2019-08-28 19:00:30 +0200

    Move format strings to progmem.

--- lagunita/level.cpp
+++ lagunita/level.cpp
@@ -310,10 +310,10 @@ void Level::onInput(Input dir) {
   case Input::b:
     if (tutorVisible) {
       if (inStats) {
-        snprintf(tutor, tutorLen,                               /**/
-                 "\nHAPPINESS\n%4d %%\nSAFETY\n%4d %%\n"        /**/
-                 "SPIRITUALITY\n%4d %%\nENVIRONMENT\n%4d %%\n", /**/
-                 happiness, safety, spirituality, environment); /**/
+        snprintf_P(tutor, tutorLen,
+                   PSTR("\nHAPPINESS\n%4d %%\nSAFETY\n%4d %%\n"
+                        "SPIRITUALITY\n%4d %%\nENVIRONMENT\n%4d %%\n"),
+                   happiness, safety, spirituality, environment);
         inStats = false;
       } else {
         tutorVisible = false;
@@ -321,10 +321,10 @@ void Level::onInput(Input dir) {
     } else {
       inStats = true;
       tutorVisible = true;
-      snprintf(tutor, tutorLen,                             /**/
-               "\nHOUSING\n%7d\nJOBS   FOOD\n%4d%7d\n"      /**/
-               "MAINTENANCE\n%7d $/s\nEARNINGS\n%4d $/s\n", /**/
-               housing, jobs, food, maintenance, earnings); /**/
+      snprintf_P(tutor, tutorLen,
+                 PSTR("\nHOUSING\n%7d\nJOBS   FOOD\n%4d%7d\n"
+                      "MAINTENANCE\n%7d $/s\nEARNINGS\n%4d $/s\n"),
+                 housing, jobs, food, maintenance, earnings);
     }
     break;
   default:
@@ -450,29 +450,29 @@ void Level::update() {
           buildingEnabled[(uint8_t)(Building::IDs::sheriff)]) {
         /* If the safety is low, simulate a robbery. */
         r = rand() % (money / 2);
-        snprintf(tutor, tutorLen,                   /**/
-                 "\nYOU HAVE\nBEEN ROBBED!\n"       /**/
-                 "\nTHE THIEVES\nSTOLE %4d$\n"      /**/
-                 "\nBUILD MORE\nSHERIFF\nPOSTS!\n", /**/
-                 r);                                /**/
+        snprintf_P(tutor, tutorLen,
+                   PSTR("\nYOU HAVE\nBEEN ROBBED!\n"
+                        "\nTHE THIEVES\nSTOLE %4d$\n"
+                        "\nBUILD MORE\nSHERIFF\nPOSTS!\n"),
+                   r);
         tutorVisible = true;
         money -= r;
       } else if ((spirituality < 100) && (!r) &&
                  buildingEnabled[(uint8_t)(Building::IDs::church)]) {
         /* If the spirituality is low, simulate emigration. */
         r = rand() % (population / 2);
-        snprintf(tutor, tutorLen, /**/
-                 "\n%4d PEOPLE\n" /**/
-                 "LOST FAITH\n"   /**/
-                 "IN LAGUNITA\n"  /**/
-                 "AND DECIDED\n"  /**/
-                 "TO FOUND\n"     /**/
-                 "THEIR OWN\n"    /**/
-                 "TOWN.\n"        /**/
-                 "BUILD MORE\n"   /**/
-                 "CHURCHES TO\n"  /**/
-                 "RISE FAITH!\n", /**/
-                 r);              /**/
+        snprintf_P(tutor, tutorLen,
+                 PSTR("\n%4d PEOPLE\n"
+                      "LOST FAITH\n"
+                      "IN LAGUNITA\n"
+                      "AND DECIDED\n"
+                      "TO FOUND\n"
+                      "THEIR OWN\n"
+                      "TOWN.\n"
+                      "BUILD MORE\n"
+                      "CHURCHES TO\n"
+                      "RISE FAITH!\n"),
+                 r);
         tutorVisible = true;
         population -= r;
       } else {
@@ -564,10 +564,10 @@ void Level::render() {
   tinyfont.setCursor(0, 5);
   tinyfont.print(itoa(5 * Building::at(sel).cost, tmp_str, 10));
 
-  snprintf(tmp_str, 16, "%9d$", money);
+  snprintf_P(tmp_str, 16, PSTR("%9d$"), money);
   tinyfont.setCursor(78, 0);
   tinyfont.print(tmp_str);
-  snprintf(tmp_str, 16, "%9dP", population);
+  snprintf_P(tmp_str, 16, PSTR("%9dP"), population);
   tinyfont.setCursor(78, 5);
   tinyfont.print(tmp_str);
 
