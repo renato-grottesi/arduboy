f1365cefc3483503fa6b70139b93f6adb4f3f21d
renato_grottesi <renato.grottesi@gmail.com>
2019-08-30 19:47:32 +0200

    more file renaming

--- lagunita/utility.hpp
+++ lagunita/utility.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
 /* List of headers included by all the other headers. */
-#include "Tinyfont.h"
+#include "Tinyfont.hpp"
 #include <Arduboy2.h>
 #include <avr/io.h>
 #include <avr/pgmspace.h>
